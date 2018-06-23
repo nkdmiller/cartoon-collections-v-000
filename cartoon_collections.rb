1c0e46a9097b120fb10de6c60b15219089b2d0c1
Nate Miller <natekmiller96@gmail.com>
2018-06-23 04:39:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map do |element|
+  planeteer_calls.map! do |element|
     (element.capitalize).concat("!")
   end
   return planeteer_calls
