cbfea8f0d024a9f7c8e59bf4caf2cbfa8fd0b0fd
Nate Miller <natekmiller96@gmail.com>
2018-06-23 04:55:27 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ end
 def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
   ingredients.each do |item|
-    if cheese_types.include(item)
+    if cheese_types.include?(item)
       return "#{item}"
     end
   end
