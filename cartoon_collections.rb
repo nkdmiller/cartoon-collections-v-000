ca505382a377d6791645fc14b27ede81617c35fd
Nate Miller <natekmiller96@gmail.com>
2018-06-23 04:36:14 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |element|
-    (element.capitalize).append("!")
+    (element.capitalize).concat("!")
   end
   return planeteer_calls
 end
