baa1a1d6e052d6de04a087bdf134aaea12c55f36
Nate Miller <natekmiller96@gmail.com>
2018-06-23 04:44:06 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,13 @@ def summon_captain_planet(planeteer_calls)
   return planeteer_calls
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.each do |call|
+    if call.length > 3 
+      return true
+    end
+  end
+  return false
 end
 
 def find_the_cheese# code an argument here
